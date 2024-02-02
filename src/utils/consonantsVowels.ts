839941c1f9aa2d20ff1ec18d2b0262785606765f
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-02-02 12:41:25 +0100

    Fix adding spaces

--- src/utils/consonantsVowels.ts
+++ src/utils/consonantsVowels.ts
@@ -1,21 +1,18 @@
-const isVowel = (ch: string) => {
-  return !!["a", "e", "i", "o", "u"].find(
-    (vowel) => vowel === ch.toLowerCase()
-  );
-};
+export const isAlpha = (ch: string) => /^[A-Za-z]$/i.test(ch);
+export const isVowel = (ch: string) => /^[aiueo]$/i.test(ch);
 
 const consonantsVowels = (str: string) => {
   const output: string[] = [];
 
   // find consonants first
   for (const ch of str) {
-    if (isVowel(ch)) continue;
+    if (isVowel(ch) || !isAlpha(ch)) continue;
     output.push(ch);
   }
 
   // now find vowels
   for (const ch of str) {
-    if (!isVowel(ch)) continue;
+    if (!isVowel(ch) || !isAlpha(ch)) continue;
     output.push(ch);
   }
 
