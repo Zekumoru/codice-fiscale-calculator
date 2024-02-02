ef52fbc4cad2795c48464338c5fdbfa52677b105
Zekumoru Dragonhart <57686690+Zekumoru@users.noreply.github.com>
2024-02-02 02:39:05 +0100

    Deploy build

--- README.md
+++
@@ -1,3 +0,0 @@
-# Codice Fiscale Calculator
-
-A simple web app to calculate codice fiscale.
--- src/utils/getMonthCode.ts
+++
@@ -1,5 +0,0 @@
-const getMonthCode = (month: number): string | undefined => {
-  return ["A", "B", "C", "D", "E", "H", "L", "M", "P", "R", "S", "T"][month];
-};
-
-export default getMonthCode;
